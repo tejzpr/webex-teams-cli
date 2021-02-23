09e681f1c529f031304cb7f7fd10a991b789576a
Teju <tejzpr@gmail.com>
2021-02-23 09:21:55 -0800

    Update main.go

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func main() {
 
 		webex-teams-cli room msg -t "message text" -f <file>
 		`,
-		Version: "v0.1b",
+		Version: "v1.0",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:     "accessToken",
